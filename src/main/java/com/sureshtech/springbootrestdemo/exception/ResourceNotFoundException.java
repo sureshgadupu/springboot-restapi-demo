e72476cdb4f7e718557c4614bc2002f3288d5bd8
sureshgadupu <gskforums@yahoo.com>
2019-12-06 22:39:18 +1300

    Exception handling by ResourceStatusException class

--- src/main/java/com/sureshtech/springbootrestdemo/exception/ResourceNotFoundException.java
+++ src/main/java/com/sureshtech/springbootrestdemo/exception/ResourceNotFoundException.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 499804821811671335L;
+
 	public ResourceNotFoundException() {
 		super();
 		// TODO Auto-generated constructor stub
--- src/main/java/com/sureshtech/springbootrestdemo/service/PostService.java
+++ src/main/java/com/sureshtech/springbootrestdemo/service/PostService.java
@@ -1,18 +1,13 @@
 package com.sureshtech.springbootrestdemo.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sureshtech.springbootrestdemo.entity.Comment;
 import com.sureshtech.springbootrestdemo.entity.Post;
@@ -35,18 +30,23 @@ public class PostService {
 	
 	public Post createPost( Post post)
 	{
-		return postRepository.save(post);
+		try {
+			return postRepository.save(post);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Exception while creating Post",e);
+		}
 	}
 	
 	
 	public Post getPost(Integer id) {
-		return postRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("Resource Not found") );
+		return postRepository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Resource Not found") );
 	}
 	
 	
 	public Post updatePost(Post post){
 		
-		postRepository.findById(post.getId()).orElseThrow( () -> new ResourceNotFoundException("Resource Not found") );
+		postRepository.findById(post.getId()).orElseThrow( () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Resource Not found") );
 		return postRepository.save(post);
 		
 	}
@@ -54,7 +54,7 @@ public class PostService {
 	
 	public @ResponseBody void deletePost(Integer id){
 		
-		postRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("Resource Not found") );		
+		postRepository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Post Not found :"+id) );		
 		postRepository.deleteById(id);
 		
 	}
@@ -62,7 +62,7 @@ public class PostService {
 	
 	public Post createPostComments(Integer postId, Comment comment) {
 		
-		Post post = postRepository.findById(postId).orElseThrow( () -> new ResourceNotFoundException("Resource Not found :"+postId) );
+		Post post = postRepository.findById(postId).orElseThrow( () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Post Not found :"+postId) );
 		
 		comment.setPost(post);
 		post.getComments().add(comment);
@@ -75,8 +75,8 @@ public class PostService {
 	
 	public void deletePostComments(Integer postId,  Integer commentId) {
 		
-		postRepository.findById(postId).orElseThrow( () -> new ResourceNotFoundException("Post Not found :"+postId) );
-		commentRepository.findById(commentId).orElseThrow( () -> new ResourceNotFoundException("Comment Not found :"+commentId) );
+		postRepository.findById(postId).orElseThrow( () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Post Not found :"+postId) ); //new ResourceNotFoundException("Post Not found :"+postId)
+		commentRepository.findById(commentId).orElseThrow( () -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Comment Not found :"+commentId) );
 		commentRepository.deleteById(commentId);
 		
 		
