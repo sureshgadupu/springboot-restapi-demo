61400d0b0313ca58f77894ec65170399701c7d91
sureshgadupu <gskforums@yahoo.com>
2019-12-26 22:50:23 +1300

    JsonviewController unit tests added

--- src/test/java/com/sureshtech/springbootrestdemo/PostJsonViewControllerTest.java
+++ src/test/java/com/sureshtech/springbootrestdemo/PostJsonViewControllerTest.java
@@ -3,8 +3,7 @@ package com.sureshtech.springbootrestdemo;
 
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -17,8 +16,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sureshtech.springbootrestdemo.controller.PostJsonViewController;
 import com.sureshtech.springbootrestdemo.entity.Post;
+import com.sureshtech.springbootrestdemo.views.PostView;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
@@ -40,46 +44,74 @@ public class PostJsonViewControllerTest {
 	}
 	
 	@Test	
-	public void testGetPostForExternal() {
+	public void testGetPostForExternal() throws JsonProcessingException {
 		
 		ResponseEntity<Post> response = 	restTemplate.getForEntity(URL+"posts/external/10", Post.class);
 		
 		Post post = (Post) response.getBody();
-		assertNotNull(post.getTitle());
-		assertNotNull(post.getContent());
-		assertNotNull(post.getId());
 		
-		assertNull(post.getCreatedOn());
+		ObjectMapper mapper = new ObjectMapper();
 		
-		assertEquals(0,post.getComments().size());
+		String result = mapper
+			      .writerWithView(PostView.ExternalView.class)
+			      .writeValueAsString(post);
+		
+		//assertThat(result, containsString("book"));
+		assertThat(result, containsString("id"));
+		assertThat(result, containsString("title"));
+		assertThat(result, containsString("content"));
+		assertThat(result, not(containsString("createdOn")));
+		
+
 		
 	}
 	
+	
+
 	@Test	
-	public void testGetPostForInternal() {
+	public void testGetPostForInternal() throws JsonProcessingException {
 		
 		ResponseEntity<Post> response = 	restTemplate.getForEntity(URL+"posts/internal/10", Post.class);
 		
 		Post post = (Post) response.getBody();
-		assertNotNull(post.getTitle());
-		assertNotNull(post.getContent());
-		assertNotNull(post.getId());
-		assertNotNull(post.getCreatedOn());
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String result = mapper.writerWithView(PostView.InternalView.class).writeValueAsString(post);
+		post = null;
+		post = mapper.readerWithView(PostView.InternalView.class).forType(Post.class).readValue(result);
+		assertThat(result, containsString("id"));
+		assertThat(result, containsString("title"));
+		assertThat(result, containsString("content"));
+		assertThat(result, containsString("updatedOn"));
+		
+		assertThat(result, (containsString("createdOn")));
 		
 		
 	}
 	
 	
 	@Test	
-	public void testGetPostForPartner() {
+	public void testGetPostForPartner() throws JsonProcessingException {
 		
 		ResponseEntity<Post> response = 	restTemplate.getForEntity(URL+"posts/partner/10", Post.class);
 		
 		Post post = (Post) response.getBody();
-		assertNotNull(post.getTitle());
-		assertNotNull(post.getContent());
-		assertNotNull(post.getId());
-	
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		String result = mapper.writerWithView(PostView.PartnerView.class).writeValueAsString(post);
+				
+		post = null;
+		
+		post = mapper.readerWithView(PostView.PartnerView.class).forType(Post.class).readValue(result);
+		
+		assertThat(result, containsString("id"));
+		assertThat(result, containsString("title"));
+		assertThat(result, containsString("content"));
+		assertThat(result, containsString("updatedOn"));
+		assertThat(result, not(containsString("createdOn")));
+		
+		
 		
 	}
 
